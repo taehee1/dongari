29b2b036d3f3338f2e14821b7132716699d2ec00
ghdrudtj <163072081+ghdrudtj@users.noreply.github.com>
2024-07-01 17:09:50 +0900

    크레딧 수정

--- Assets/2_Script/Credit.cs
+++ Assets/2_Script/Credit.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class Credit : MonoBehaviour
 {
-    public GameObject returnBtn;
     public void ReturnBtn()
     {
         SceneManager.LoadScene("StartScene");
+    }
+    void Start()
+    {
+
     }
 
     void Update()
