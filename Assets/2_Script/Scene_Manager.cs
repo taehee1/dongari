ddc1a151bb2d6b029296b0916aab657d51a8c264
ghdrudtj <ghdqwer432@gmail.com>
2024-07-04 11:59:14 +0900

    타이틀 씬의 씬 매니저 수정

--- Assets/2_Script/Scene_Manager.cs
+++ Assets/2_Script/Scene_Manager.cs
@@ -7,13 +7,10 @@ public class Scene_Manager : MonoBehaviour
 {
     public void StartScene()
     {
-        SceneManager.LoadScene("StartScene");
+        SceneManager.LoadScene("MainScene");
     }
 
-    public void MainScene()
-    {
-        SceneManager.LoadScene("Main");
-    }
+    
 
     public void BossScene()
     {
