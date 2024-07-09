7b6d67b7c3d009b427b4a7322a19ada3c1208b60
leechuanhung <163071930+taehee1@users.noreply.github.com>
2024-07-09 15:18:09 +0900

    보스씬이동

--- Assets/2_Script/Scene_Manager.cs
+++ Assets/2_Script/Scene_Manager.cs
@@ -5,21 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class Scene_Manager : MonoBehaviour
 {
+    public static Scene_Manager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
     public void StartScene()
     {
         SceneManager.LoadScene("MainScene");
     }
 
-    
-
     public void BossScene()
     {
         SceneManager.LoadScene("BossScene");
     }
+
     public void TilteScene()
     {
         SceneManager.LoadScene("StartScene");
     }
+
     public void ExitGame()
     {
         Application.Quit();
